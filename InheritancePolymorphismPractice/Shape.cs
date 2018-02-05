e15192f20f7491a1bdb5649aa28b8c434fe49d4c
RJ Kline <rj.kline@gmail.com>
2018-02-05 11:40:37 -0500

    first commit

---
+++ InheritancePolymorphismPractice/Shape.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePolymorphismPractice
+{
+    class Shape
+    {
+        //fields
+        private int sides;
+
+        //properties
+        public int Sides
+        {
+            get { return this.sides; }
+            set { this.sides = value; }
+        }
+
+        //Consturctors
+
+    
+    }
+}

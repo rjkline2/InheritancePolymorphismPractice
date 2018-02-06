c004fbd9878918574dca929c86696ab42044f2e3
RJ Kline <rj.kline@gmail.com>
2018-02-06 11:15:06 -0500

    Project Update

---
+++ InheritancePolymorphismPractice/Membership.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePolymorphismPractice
+{
+    public class Membership
+    {
+        //fields
+        private int memberID;
+        private bool isActive;
+
+        //properties
+        public int MemberID
+        {
+            get { return this.memberID; }
+            set { this.memberID = value; }
+        }
+        public bool IsActive
+        {
+            get { return this.isActive; }
+            set { this.isActive = value; }
+        }
+        
+
+        //constructors
+        public Membership()
+        {
+            //default constructor
+        }
+        public Membership(int memberID, bool isActive)
+        {
+            this.memberID = memberID;
+            this.isActive = isActive;
+        }
+
+        //methods
+        public virtual void Activate()
+        {
+            if (isActive == false)
+            {
+                isActive = true;
+                Console.WriteLine("Your membership has been activated.");
+            }
+            else
+            {
+                Console.WriteLine("Your membership is already active.");
+            }
+        }
+
+    }
+
+}
---
+++ InheritancePolymorphismPractice/Standard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePolymorphismPractice
+{
+    public class Standard : Membership
+    {
+        //fields
+        //invisible
+
+        //Properties
+        public int Cost { get; set; }
+
+        //Constructors
+        public Standard()
+        {
+            //default constructor
+        }
+        public Standard(int cost) //overloaded constructor
+        {
+            Cost = cost; //instead of using the this.thingy = thing value thing....
+        }
+
+        //Methods
+        public override void Activate()
+        {
+            base.Activate();
+        }
+    }
+}
